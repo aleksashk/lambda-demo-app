43529a61e8ccb4c13d163fcc5f088fa29150654a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-23 20:30:16 +0300

    create 2 threads

--- src/test/java/by/flameksandr/LambdaTest.java
+++ src/test/java/by/flameksandr/LambdaTest.java
@@ -15,4 +15,23 @@ public class LambdaTest {
         List<Person> people = DataService.getPeople();
         assertEquals(people.size(), 1000);
     }
+
+    @Test
+    public void threadsLambdas() {
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running thread r1;");
+            }
+        };
+        Runnable r2 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Running thread r2;");
+            }
+        };
+
+        new Thread(r1).start();
+        new Thread(r2).start();
+    }
 }
