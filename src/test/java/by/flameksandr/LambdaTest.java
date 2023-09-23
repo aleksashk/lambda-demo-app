618661c0c69b1333648f6eabf4390254c7e2c457
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-09-23 20:33:37 +0300

    create 2 threads with lambda expression

--- src/test/java/by/flameksandr/LambdaTest.java
+++ src/test/java/by/flameksandr/LambdaTest.java
@@ -18,17 +18,30 @@ public class LambdaTest {
 
     @Test
     public void threadsLambdas() {
-        Runnable r1 = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Running thread r1;");
-            }
+//        Runnable r1 = new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Running thread r1;");
+//            }
+//        };
+//        Runnable r2 = new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Running thread r2;");
+//            }
+//        };
+//
+//        new Thread(r1).start();
+//        new Thread(r2).start();
+
+        //functional interface
+
+        Runnable r1 = () -> {
+            System.out.println("Running thread r1");
         };
-        Runnable r2 = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Running thread r2;");
-            }
+
+        Runnable r2 = () -> {
+            System.out.println("Running thread r2");
         };
 
         new Thread(r1).start();
